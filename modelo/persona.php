10da96e47d3abe657b43dddb1b156ba704805a52
Enyelberth Rodriguez <enyelberthrc22.z@gmail.com>
2023-12-21 23:28:23 -0400

    das
    dadasd

--- modelo/persona.php
+++ modelo/persona.php
@@ -1,5 +1,13 @@
 <?php 
-
+class Persona{
+    private $nombre;
+    private $apelldio;
+    private $cedula;
+     
+    public function __construct($name,$apellido){
+        $this->
+    }
+}
 
 
 
